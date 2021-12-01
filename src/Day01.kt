0710f97ad3e0b41aa6608c80d4da42c1bdfa9079
curiousNikhil <nikhyl777@gmail.com>
2021-12-01 17:28:57 +0530

    [Day01] Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.windowed(2)
+            .count { (firstScan, secondScan) -> secondScan.toInt() > firstScan.toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.windowed(3) { window -> window.sumOf { it.toInt() } }
+            .windowed(2)
+            .count { (firstScan, secondScan) -> secondScan > firstScan }
+
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
+
+//    println(part1(input))
     println(part2(input))
 }
